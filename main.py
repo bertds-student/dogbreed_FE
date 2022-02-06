b0b5035caaee32083238d9e105e8863b1f73da90
bertds-student <bert.desutter@gmail.com>
2022-02-06 11:54:38 +0100

    hond extra info

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ from PIL import Image
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
 # defines an h1 header
-st.title("Dogbreed detection web app")
+st.title("Hondenasiel.be - Hond zoekt nieuw baasje")
+image = Image.open(https://xxlfotobehang.nl/wp-content/uploads/2016/10/XXL-D-10.jpg)
+st.image(image, caption='Deze honden hebben een goede thuis gevonden.')
+
+st.Header("Honden info :")
+naam_hond = st.text_input('Puk')
+st.write('Naam : ', naam_hond)
+
 
 # displays a file uploader widget
-image = st.file_uploader("Choose an image")
+image = st.file_uploader("Laad hier een foto van je hond op : ")
 
 # displays a button
-if st.button("Detect dogbreed"):
+if st.button("Bepaal het ras obv de foto"):
     if image is not None :
         files = {"file": image.getvalue()}
         st.image(image, width=500)
